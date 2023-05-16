de8cbc54c848ae821d5f4eaabea5be2e175736aa
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-05-16 07:56:17 +0300

    was able to do all except the number 5

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+     print ("Hello, progarammer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print (f"Hello, {name}!")
+greet("augasha")
 
 def greet_with_default(name="programmer"):
-    pass
+    print (f"Hello, {name}!")
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    print (num1 + num2)
+add(4,4)
 
 def halve(number):
-    pass
+    if number != "number":
+        print ("null") 
+halve (100 / 2)
